98a6e513b1f963625ba9640765c738dc2de3349d
SangiliKutty <sangilisangili687@gmail.com>
2026-06-30 00:19:15 +0530

    feat: add serial blink counter

--- week1/led_blink.ino.ino
+++ week1/led_blink.ino.ino
@@ -1,10 +1,19 @@
+int count = 0;
+
 void setup() {
   pinMode(13, OUTPUT);
+  Serial.begin(9600);
 }
 
 void loop() {
   digitalWrite(13, HIGH);
   delay(500);
+
   digitalWrite(13, LOW);
   delay(500);
+
+  count++;
+
+  Serial.print("Blink Count: ");
+  Serial.println(count);
 }
